c05d8860cbe0a472525311bc56cb2d1728fe752d
angel <reysi2003@gmail.com>
2022-04-06 11:33:55 -0400

    adding the new folder

---
+++ Python/helloHeal.py
@@ -0,0 +1,8 @@
+
+name = "Angel"
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+print("Hello " + name + " wellcome to Heal! " + str(number) + " are your numbers")
+print(str(sum(number)) + " all together")
+
