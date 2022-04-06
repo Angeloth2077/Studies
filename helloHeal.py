9e4ceeab9d2d45e0726aa3bc8a7441f7d1afaa3e
angel <reysi2003@gmail.com>
2022-04-06 11:24:07 -0400

    preparign main for a conflict again

--- helloHeal.py
+++ helloHeal.py
@@ -2,6 +2,7 @@
 name = "Angel"
 number = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+
 print("Hello " + name + " wellcome to Heal! " + str(number) + " are your numbers")
 print(str(sum(number)) + " all together")
 
